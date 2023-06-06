98b9ef4311b557da0d5f4c2badc7868165251863
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-06-06 18:09:24 +0100

    remix configuration

---
+++ remix.config.js
@@ -0,0 +1,17 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  tailwind: true,
+  serverDependenciesToBundle: ["@remix-pwa/sw", "@remix-pwa/push"],
+  future: {
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+};
